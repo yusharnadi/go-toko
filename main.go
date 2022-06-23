612b07f33ca3299026676b554378e46f8e542fcd
yusharnadi <yust.44@gmail.com>
2022-06-23 10:30:41 +0700

    add gorm driver mysql

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
 func main() {
 
+	dsn := "root:@tcp(127.0.0.1:3306)/go_toko?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Setup repository
+	// productRepository := repository.NewProductRepository(db)
 }
