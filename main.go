b548903971e79c0ace0a714e3581fee5d8792169
yusharnadi <yust.44@gmail.com>
2022-06-23 21:17:56 +0700

    add handlebar template

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"log"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/handlebars"
+	"github.com/yusharnadi/go-toko/controller"
+	"github.com/yusharnadi/go-toko/repository"
+	"github.com/yusharnadi/go-toko/service"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,14 +15,35 @@ import (
 func main() {
 
 	dsn := "root:@tcp(127.0.0.1:3306)/go_toko?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
+	// Create a new engine
+	engine := handlebars.New("./views", ".hbs")
+
 	// Setup repository
-	// productRepository := repository.NewProductRepository(db)
+	productRepository := repository.NewProductRepository(db)
 
 	//setup Service
-	//  productService := service.NewProductService(productRepository)
+	productService := service.NewProductService(productRepository)
+
+	//setup Controller
+	productController := controller.NewProductController(productService)
+
+	// Setup Fiber
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Static("/static/", "./assets")
+
+	// Setup Routing
+	productController.Route(app)
+
+	// Start App
+	errors := app.Listen(":3000")
+	if errors != nil {
+		log.Fatal(errors.Error())
+	}
 }
