3ac0c020af03202a21900012ce26c29fcbedde96
yusharnadi <yust.44@gmail.com>
2022-06-23 10:30:05 +0700

    fix int type

--- entity/product.go
+++ entity/product.go
@@ -5,8 +5,8 @@ import "time"
 type Product struct {
 	ID        uint64
 	Name      string
-	Price     int64
-	Stock     int32
+	Price     int
+	Stock     int
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
